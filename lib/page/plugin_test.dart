51fdcefb10005bfc1d978d0409b8180c5d55d2f8
liuhao <liu.hao2021@outlook.com>
2022-01-07 10:49:08 +0800

    添加包

---
+++ lib/page/plugin_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PluginTest extends StatelessWidget {
+  const PluginTest({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('测试'),
+      ),
+    );
+  }
+}
