959228b37da7c4c69b3e30a93f8e983f6aa06cd8
liuhao <liu.hao2021@outlook.com>
2022-01-07 10:31:28 +0800

    修改界面名称

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:plugdemo/main.dart';
+import 'package:plugdemo/plug_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
